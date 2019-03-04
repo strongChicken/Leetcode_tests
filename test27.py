5097921a092a555bd260afd7235c97cfabe5fb1e
strongChicken <mrwater93@163.com>
2019-03-05 00:45:38 +0800

    done

--- test27.py
+++ test27.py
@@ -1,8 +1,15 @@
 def removeElement(self, nums: List[int], val: int) -> int:
-    if len(nums) <= 1 and nums[0] != val:
-        return len(nums)
+    if len(nums) == 0:
+        return 0
 
     _s = 0
 
-    if nums[0] != val:
-        for i in range(1, len(nums)):
+    for f in range(0,len(nums)):    # f 一直遍历整个List，
+        if nums[f] != val:          # 当nums[f] == val时，f指针跳过 当前元素，继续遍历。直到找到一个不等于val的元素的位置（new)f
+            nums[_s] = nums[f]      # 将不等于val的元素赋值给 _s 位置的元素，只要 f 位置的元素不等于 val，就会持续赋值。——
+            _s += 1                 # ——即使后续有连续元素 等于 val，但持续的赋值会保持 （!=val）的元素连续覆盖。
+                                    # _s += 1保证了指针有序的遍历，f指针把合适的元素赋值（覆盖）_s 位置的元素
+
+    return _s
+
+
