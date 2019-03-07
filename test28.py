0204def34205c57c1a9184f04c1d5d94660eb8ad
strongChicken <mrwater93@163.com>
2019-03-08 01:33:11 +0800

    wait for improving

--- test28.py
+++ test28.py
@@ -1,17 +1,28 @@
+# 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
 def strStr(haystack: str,needle: str):
+    if (haystack == "") or (needle == ""):
+        return 0
+
+    HS_L = len(haystack)
+    ND_L = len(needle)
+
     ND_Point = 0
+    HS_Point = 0
 
-    same = 0
-    diff = 0
-
-    for Hs_Point in range(len(haystack)):
-        if haystack[Hs_Point] == needle[ND_Point]:  #如何处理ND_Point的边界问题
-            ND_Point += 1                           #当needle已经遍历完，haystack还没有遍历完
-            same += 1
+    while((HS_Point < HS_L) and (ND_Point < ND_L)):
+        if haystack[HS_Point] != needle[ND_Point]:
+            HS_Point = HS_Point - ND_Point + 1  # 用HS指针位置 - ND指针位置 会得到 HS原指针位置，事实上需要在HS[原位置+1]的位置开始继续配对
+            ND_Point = 0
         else:
-            ND_Point == 0
-            diff += 1
-    return ND_Point
+            HS_Point += 1
+            ND_Point += 1
 
-print(strStr("hello", "ll"))
+
+    if ND_Point == ND_L:
+        return HS_Point - ND_Point  # 返回needle 出现的第一个位置
+    else:
+        return -1
+
+print(strStr("", "a"))
 
