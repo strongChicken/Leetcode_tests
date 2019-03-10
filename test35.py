cb75bd81fee39cc5ad77da08c1a70da06cf69fcd
strongChicken <mrwater93@163.com>
2019-03-11 00:40:47 +0800

    正在解决二分法

--- test35.py
+++ test35.py
@@ -1,2 +1,18 @@
-def searchInsert(nums:list[int], target:int):
-    
+def searchInsert(nums:list, target:int):
+    L_List = 0
+    R_List = len(nums)
+
+    while(L_List > R_List):
+        mid = (R_List - L_List) // 2
+        if target > nums[mid]:
+            L_List = mid
+
+        elif target < nums[mid]:
+            R_List = mid
+        else:
+            return  mid
+
+
+test = [1,2,3,4]
+T = 3
+print(searchInsert(test,T))
