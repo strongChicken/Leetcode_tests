b2f7c156b12f3048a3f5debcd05a955a0c29bbe8
strongChicken <mrwater93@163.com>
2019-03-12 00:07:39 +0800

    仅完成了二分法的学习，最终返回结果，还没有理解

--- test35.py
+++ test35.py
@@ -1,18 +1,19 @@
 def searchInsert(nums:list, target:int):
     L_List = 0
-    R_List = len(nums)
+    R_List = len(nums) - 1
 
-    while(L_List > R_List):
-        mid = (R_List - L_List) // 2
+    while(L_List <= R_List):
+        mid = (R_List + L_List) // 2
         if target > nums[mid]:
-            L_List = mid
-
+            L_List = mid + 1
         elif target < nums[mid]:
-            R_List = mid
-        else:
-            return  mid
+            R_List = mid - 1
+        elif target == nums[mid]:
+            return mid
 
+    # return L_List         # 为什么返回左边界？
 
-test = [1,2,3,4]
-T = 3
+test = [1,3,4,5,7]
+T = 8
+
 print(searchInsert(test,T))
